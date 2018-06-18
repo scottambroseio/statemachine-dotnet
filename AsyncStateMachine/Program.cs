5e65d80f47a62ec8f376ec663dc0fea5b5cf1687
Scott Ranger <scottranger@hotmail.co.uk>
2018-06-18 16:45:39 +0100

    Add project files.

---
+++ AsyncStateMachine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AsyncStateMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
